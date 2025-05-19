0f6dbd0f97fb1d59d106335aaa8f8adf363a829a
ST10476635 <ST10476635@imconnect.edu.za>
2025-05-19 21:11:18 +0200

    3rd Screen
    Finished the Score sheet that displays all the feedback

--- MyQuestion/app/src/main/java/vcmsa/ci/myquestion/ScoreActivity.kt
+++ MyQuestion/app/src/main/java/vcmsa/ci/myquestion/ScoreActivity.kt
@@ -1,6 +1,9 @@
 package vcmsa.ci.myquestion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,37 @@ class ScoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_score)
+
+        val score = intent.getIntArrayExtra("SCORE",0)
+        val totalQuestions = intent.getIntArrayExtra("TOTAL QUESTIONS",5)
+
+        val scoreText = findViewById<TextView>(R.id.txtScore)
+        val feedBackText = findViewById<TextView>(R.id.txtFeedback)
+        val reviewButton = findViewById<Button>(R.id.btnReview)
+        val exitButton = findViewById<Button>(R.id.btnExit)
+
+        scoreText.text = "Your Score: $score/$totalQuestions"
+
+        val feedback = when{
+            score => 4 -> "Excellent Work! You know your stuff."
+
+            score => 3 -> "Good job! You got a decent memory"
+
+            else -> "Keep practicing! History is important."
+        }
+        feedBackText.text = feedback.toString()
+
+        reviewButton.setOnClickListener {
+            val intent = Intent(this,ReviewActivity::class.java)
+            startActivity(intent)// transfer user to next screen
+        }
+        exitButton.setOnClickListener {
+            finishAffinity()//Close all activities and exist app
+        }
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
