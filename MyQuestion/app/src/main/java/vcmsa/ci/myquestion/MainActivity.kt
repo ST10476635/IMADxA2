c718bbbab49b4736e5f3be61af0adb8beb94cbc6
ST10476635 <ST10476635@imconnect.edu.za>
2025-05-19 19:12:16 +0200

    First Screen finish

--- MyQuestion/app/src/main/java/vcmsa/ci/myquestion/MainActivity.kt
+++ MyQuestion/app/src/main/java/vcmsa/ci/myquestion/MainActivity.kt
@@ -1,16 +1,32 @@
 package vcmsa.ci.myquestion
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.btnStart)
+        val WelcomeScreen = findViewById<TextView>(R.id.txtWelcomeText)
+
+        WelcomeScreen.text = "Welcome to Flashcard History game!\n\nTest your knowledge of historical facts with 5 True/ false questions."
+
+        startButton.setOnClickListener {
+            val intent = Intent (this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
