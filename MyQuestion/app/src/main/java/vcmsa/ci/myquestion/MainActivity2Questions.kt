80f4290225c3f357e2ee9368ce3105dc50d9d7ad
ST10476635 <ST10476635@imconnect.edu.za>
2025-05-19 20:36:23 +0200

    Secoond screen
    Adding second screen that has private functions and include the array for the questionnaire. 3 button True, False and Next were added.

---
+++ MyQuestion/app/src/main/java/vcmsa/ci/myquestion/MainActivity2Questions.kt
@@ -0,0 +1,73 @@
+package vcmsa.ci.myquestion
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2Questions : AppCompatActivity() {
+
+    private val questions = arrayOf(
+        "Nelson Mandela was the president of South Africa in 1994","The World War II ended in 1945","The Berlin Wall fel in 1989","The Renaissance began in the 14th century","The United States declared independence in 1776"
+    )
+    private val answers = booleanArrayOf(true,true,true,true,true)
+    private var currentQuestionindex = 0
+    private var score = 0
+
+    private fun displayQuestion(){
+        val questionsText = findViewById<TextView>(R.id.txtQuestions)
+        questionsText.text = questions[currentQuestionindex]
+    }
+    private fun checkAnswer(userAnswer Boolean){
+        val isCorrect = userAnswer == answers[currentQuestionindex]
+        if (isCorrect){
+            score++
+            Toast.makeText(this,"Correct!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activity2_qustions)
+        displayQuestion()
+
+        val trueButton = findViewById<Button>(R.id.btnTrue)
+        val falseButton = findViewById<Button>(R.id.btnFalse)
+        val nextButton = findViewById<Button>(R.id.btnNext)
+
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener {
+            checkAnswer(false)
+        }
+        nextButton.setOnClickListener {
+            currentQuestionindex++
+            if (currentQuestionindex<questions.size) {
+                displayQuestion()
+            } else {
+
+                val intent = Intent(this, ScoreActivity::class.java)
+                intent.putExtras("Score", score)
+                intent.putExtras("TOTAL QUESTIONS", questions.size)
+                startActivity(intent)
+                finish()
+            }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
